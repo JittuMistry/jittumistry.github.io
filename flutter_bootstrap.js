aaea0d74acbfedf77fe57cc4137dd3348ac35c9b
Jitendra Mistry <jitendramistry@Jitendras-Air.lan>
2026-05-31 17:30:55 +0530

    fix resume url

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3192199556" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2050575251" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
